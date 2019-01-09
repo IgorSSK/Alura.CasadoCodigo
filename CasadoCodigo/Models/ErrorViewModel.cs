fffd08951cec0b902a7a7fd6c7ee6d964f3ddab8
Igor Souza <igorsouza.96@hotmail.com>
2019-01-09 17:14:18 -0200

    Adicionando Projeto

---
+++ CasadoCodigo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CasadoCodigo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
